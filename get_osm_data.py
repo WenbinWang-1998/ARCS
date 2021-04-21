8c46c1263e704651447fe918fad8dac75ca2f283
dongfang98 <78338843+dongfang98@users.noreply.github.com>
2021-04-21 11:21:30 +0800

    Add files via upload

---
+++ get_osm_data.py
@@ -0,0 +1,88 @@
+import matplotlib.pyplot as plt
+import json
+import xml.dom.minidom
+
+dom = xml.dom.minidom.parse('BostonMap.txt')
+root = dom.documentElement
+nodelist = root.getElementsByTagName('node')
+waylist = root.getElementsByTagName('way')
+node_dic = {}
+node_dic_all = {}
+name_dic = {}
+# all nodes
+for node in nodelist:
+    node_id = node.getAttribute('id')
+    node_lat = float(node.getAttribute('lat'))
+    node_lon = float(node.getAttribute('lon'))
+    node_name = 'none'
+    
+    # get node name
+    try:
+        node_taglist = node.getElementsByTagName('tag')
+        for tag in node_taglist:
+            if tag.getAttribute('k') == 'name':
+                node_name = tag.getAttribute('v')
+    except:
+        pass
+    node_dic[node_id] = (node_lat, node_lon)
+    if node_name != 'none':
+        node_dic_all[node_id] = {
+            'address': [node_lat, node_lon],
+            'name': node_name
+        }
+        name_dic[node_name] = [node_id, node_lat, node_lon]
+    else:
+        node_dic_all[node_id] = {
+            'address': (node_lat, node_lon)
+        }
+'''
+node_all文件里存放所有node的id，经纬度，和名称。
+如果该node无name，则无此项
+可以用于根据id查询所有node的lat,lon,name
+数据结构："61340495": {"address": [42.3577809, -71.0702841], "name": "Ralph Cook Square"}
+'''
+with open('node_all.json', 'w') as fout:
+    json.dump(node_dic_all, fout)
+
+'''
+node_name文件只存放所有有name的node
+没有name的node不会被保存在这个文件中
+可以用于根据name查询node的id,lat,lon
+数据结构："Ralph Cook Square": ["61340495", 42.3577809, -71.0702841]
+'''
+with open('node_name.json', 'w') as fout:
+    json.dump(name_dic, fout)
+
+# get way nodes
+#node_dic2 = {}
+way_node = {}
+for way in waylist:
+    taglist = way.getElementsByTagName('tag')
+    way_id = way.getAttribute('id')
+    road_flag = False
+    for tag in taglist:
+        if tag.getAttribute('k') == 'highway':
+            road_flag = True
+            break
+    if  road_flag:
+        ndlist = way.getElementsByTagName('nd')
+        each_way_node = {}
+        for nd in ndlist:
+            nd_id = nd.getAttribute('ref')
+            node_lat = node_dic[nd_id][0]
+            node_lon = node_dic[nd_id][1]
+            #node_dic2[nd_id] = (node_lat, node_lon)
+            each_way_node[nd_id] = (node_lat, node_lon)
+    way_node[way_id] = each_way_node
+    
+#print (len(node_dic2))
+#with open('way_node_all.json', 'w') as fout:
+#    json.dump(node_dic2, fout)
+'''
+way_node用于存储所有highway及其经过的node
+可以用于查看每个node的neighbour_node
+数据结构："8637791": {"61353530": [42.3532505, -71.0788875], "61353260": [42.3538006, -71.0767435]}
+表示way_id为'8637791'的路上包含两个点'61353530'和'61353260',并显示他们的lat,lon
+'''
+with open('way_node.json', 'w') as fout:
+    json.dump(way_node, fout)
