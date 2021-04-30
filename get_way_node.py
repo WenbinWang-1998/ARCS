9bd23891f80318202bbde8f6eeb472ac76989990
dongfang98 <78338843+dongfang98@users.noreply.github.com>
2021-05-01 02:38:18 +0800

    Add files via upload

--- get_way_node.py
+++ get_way_node.py
@@ -39,6 +39,7 @@ way_node用于存储所有highway及其经过的node
 可以用于查看每个node的neighbour_node
 数据结构："8637791": {"61353530": [42.3532505, -71.0788875], "61353260": [42.3538006, -71.0767435]}
 表示way_id为'8637791'的路上包含两个点'61353530'和'61353260',并显示他们的lat,lon
-'''
+
 with open('way_node.json', 'w') as fout:
     json.dump(way_node, fout, indent = 4)
+'''
